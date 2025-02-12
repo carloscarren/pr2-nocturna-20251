228b64de205c99d0b3d9919d6137bf0d5789972c
josalazar <53492638+devjosalazar@users.noreply.github.com>
2025-02-11 19:47:16 -0500

    Update Main.java

--- co.edu.uniquindio.programacion2/src/main/java/co/edu/uniquindio/programacion2/Main.java
+++ co.edu.uniquindio.programacion2/src/main/java/co/edu/uniquindio/programacion2/Main.java
@@ -2,6 +2,13 @@ package co.edu.uniquindio.programacion2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String tipo = "celular";
+        int total = calcularTipoDispositivo(tipo);
     }
+
+    private static int calcularTipoDispositivo(String tipo) {
+        return  0;
+    }
+
+
 }
